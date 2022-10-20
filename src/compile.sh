e237d7bdb77f5b7d824198a3758ffac3e2d53efd
Mauricio Caceres <mauricio.caceres.bravo@gmail.com>
2022-10-20 12:50:48 -0400

    Fixing cross-compile install script

--- src/compile.sh
+++ src/compile.sh
@@ -12,18 +12,22 @@ cd ${ROOT}
 git clone --recursive https://github.com/embotech/ecos
 git clone --recursive https://github.com/osqp/osqp
 cd ecos
-# [ $(uname -s) == 'Darwin' ] && sed -i 's/^CFLAGS\(.*\)/CFLAGS\1 -arch amd64/g' Makefile
+# [ $(uname -s) == 'Darwin' ] && sed -i -e 's/^CFLAGS\(.*\)/CFLAGS\1 -arch amd64/g' Makefile
 make
 cd ../osqp
 if [[ $(uname -m) == 'arm64' && $(uname -s) == 'Darwin' ]]; then
-  sed -i '/^elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Darwin").*/a\ \ \ \ set(CMAKE_OSX_ARCHITECTURES "arm64" CACHE INTERNAL "" FORCE)' CMakeLists.txt
+  sed -i -e '/^elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Darwin").*/a\ \ \ \ set(CMAKE_OSX_ARCHITECTURES "arm64" CACHE INTERNAL "" FORCE)' ../CMakeLists.txt
 fi
 mkdir build
 cd build
 cmake -G "Unix Makefiles" ..
 cmake --build .
 cd ../..
-make all OSQP_H=./osqp/include OSQP_A=./osqp/build/out/libosqp.a ECOS_H="./ecos/include -I./ecos/external/SuiteSparse_config" ECOS_A="./ecos/libecos.a ./ecos/libecos_bb.a"
+if [[ $(uname -m) == 'arm64' && $(uname -s) == 'Darwin' ]]; then
+    make all OSQP_H=./osqp/include OSQP_A=./osqp/build/out/libosqp.a ECOS_H="./ecos/include -I./ecos/external/SuiteSparse_config" ECOS_A="./ecos/libecos.a ./ecos/libecos_bb.a" OSQP_OUT=src/build/honestosqp_macosxarm64.plugin ECOS_OUT=src/build/honestecos_macosxarm64.plugin
+else
+    make all OSQP_H=./osqp/include OSQP_A=./osqp/build/out/libosqp.a ECOS_H="./ecos/include -I./ecos/external/SuiteSparse_config" ECOS_A="./ecos/libecos.a ./ecos/libecos_bb.a"
+fi
 rm -rf ecos osqp
 cd ${CWD}
 exit 0
